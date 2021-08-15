8b77c20fdde2bfdfddcd7f6a4450ffd490a53e28
RalXYZ <RalXYZ@pm.me>
2021-08-15 14:49:33 +0800

    modify unsafe block

--- c-call-rust/endpoint/procedure.rs
+++ c-call-rust/endpoint/procedure.rs
@@ -3,16 +3,18 @@
 use rand::Rng;
 
 #[no_mangle]
-pub unsafe extern "C" fn rust_procedure(
+pub extern "C" fn rust_procedure(
     caller_language: *const libc::c_char,
     caller_stack: libc::c_int
 ) {
     let callee_stack = rand::thread_rng().gen_range(1..10_000);
-    let msg_str: &str = 
+
+    let msg_str: &str = unsafe {
         std::ffi::CStr
         ::from_ptr(caller_language)
         .to_str()
-        .unwrap_or("unknown");
+        .unwrap_or("unknown")
+    };
 
     println!(
         "[{:0>4}] start of a rust procedure, called by {} [{:0>4}]", 
